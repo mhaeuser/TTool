d7060decad83ef7cf9f8f0973780028a01d17b29
Ludovic Apvrille <ludovic.apvrille@telecom-paris.fr>
2023-05-16 16:17:06 +0200

    Resolving bug on test

--- ttool/src/test/java/tmltranslator/DiplodocusSimulatorTest.java
+++ ttool/src/test/java/tmltranslator/DiplodocusSimulatorTest.java
@@ -39,8 +39,8 @@ public class DiplodocusSimulatorTest extends AbstractTest {
 
     // model for Daemon Run To Next Breakpoint
     final String MODELS_DAEMON_RTNB = "testDaemon";
-    final int [] DAEMON_RTNBP_1 = {10, 9, 2147483647, 0}; // 2147483647==-1
-    final int [] DAEMON_RTNBP_2 = {17, 16, 2147483647, 0}; // 2147483647==-1
+    final int [] DAEMON_RTNBP_1 = {12, 11, 2147483647, 0}; // 2147483647==-1
+    final int [] DAEMON_RTNBP_2 = {19, 18, 2147483647, 0}; // 2147483647==-1
     private String SIM_DIR;
 
     @BeforeClass
